d34368fdd8fd1b8d4a78d1842ed7bf51e8b9a2e6
Satyam Tiwari <tiwarisatyam4685@gmail.com>
2025-05-20 14:27:46 +0530

    Day17

---
+++ Zero_Array_Transformation_I.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isZeroArray(int[] nums, int[][] queries) {
+        int n = nums.length;
+        int diff[] = new int[n+1];
+
+        for(int arr[]: queries){
+            int a = arr[0];
+            int b = arr[1];
+            diff[a]-= 1;
+            if(b+1<n){
+                diff[b+1]+= 1;
+            }
+        }
+
+        int sum = 0;
+
+        for(int i=0;i<n;i++){
+            sum+= diff[i];
+            nums[i]+= sum;
+            if(nums[i]>0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
